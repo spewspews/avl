9afbb71bacd1566bc2d4546fa9b5cafca06b16e1
Benjamin Scher Purcell <benjapurcell@gmail.com>
2017-04-05 13:08:47 -0500

    rename things

--- avl_test.go
+++ avl_test.go
@@ -4,6 +4,8 @@ import (
 	"math/rand"
 	"testing"
 	"time"
+
+	"github.com/emirpasic/gods/trees/avltree"
 )
 
 const (
@@ -121,5 +123,66 @@ func (tree *IntTree) checkOrdered(t *testing.T) {
 			t.Errorf("Tree not ordered: %d ≮ %d", tree.Value(next), tree.Value(n))
 		}
 		n = next
+	}
+}
+
+func BenchmarkLookup100(b *testing.B) {
+	benchmarkLookup(b, 100)
+}
+
+func BenchmarkLookup1000(b *testing.B) {
+	benchmarkLookup(b, 1000)
+}
+
+func BenchmarkLookup10000(b *testing.B) {
+	benchmarkLookup(b, 10000)
+}
+
+func BenchmarkLookup100000(b *testing.B) {
+	benchmarkLookup(b, 100000)
+}
+
+func BenchmarkGoDSGet100(b *testing.B) {
+	benchmarkGoDSGet(b, 100)
+}
+
+func BenchmarkGoDSGet1000(b *testing.B) {
+	benchmarkGoDSGet(b, 1000)
+}
+
+func BenchmarkGoDSGet10000(b *testing.B) {
+	benchmarkGoDSGet(b, 10000)
+}
+
+func BenchmarkGoDSGet100000(b *testing.B) {
+	benchmarkGoDSGet(b, 100000)
+}
+
+func benchmarkLookup(b *testing.B, size int) {
+	b.StopTimer()
+	var tree IntTree
+	Make(&tree)
+	for n := 0; n < size; n++ {
+		tree.Insert(n)
+	}
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		for n := 0; n < size; n++ {
+			tree.Lookup(n)
+		}
+	}
+}
+
+func benchmarkGoDSGet(b *testing.B, size int) {
+	b.StopTimer()
+	tree := avltree.NewWithIntComparator()
+	for n := 0; n < size; n++ {
+		tree.Put(n, nil)
+	}
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		for n := 0; n < size; n++ {
+			tree.Get(n)
+		}
 	}
 }
